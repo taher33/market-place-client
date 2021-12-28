76fb0be0fce46d7070b756cbabbd0a2df43560bf
taher33 <taherhehe@gmail.com>
2021-12-28 19:43:12 +0100

    should work 3

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axios_instance = (withCredentials = false) =>
   axios.create({
     baseURL:
-      process.env.REACT_APP_ENV === "dev"
+      process.env.NODE_ENV === "development"
         ? process.env.REACT_APP_API_ENDPOINT_DEV
         : process.env.REACT_APP_API_ENDPOINT_PROD,
     withCredentials: withCredentials,
